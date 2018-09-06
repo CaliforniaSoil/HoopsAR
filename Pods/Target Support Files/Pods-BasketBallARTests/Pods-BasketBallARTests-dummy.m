1994e4aba5aabbb19937c176769e23a2f6ff08ca
Jason Lee <jason@Jasons-MBP.attlocal.net>
2018-09-05 21:14:00 -0700

    detect plane, place hoop, and tap and hold to shoot ball

---
+++ Pods/Target Support Files/Pods-BasketBallARTests/Pods-BasketBallARTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BasketBallARTests : NSObject
+@end
+@implementation PodsDummy_Pods_BasketBallARTests
+@end
---
+++ Pods/Target Support Files/Pods-BasketBallARUITests/Pods-BasketBallARUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BasketBallARUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BasketBallARUITests
+@end
